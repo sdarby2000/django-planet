347029fe15c9d6cb1bfe114acf6f1df7abfd22cf
Stephen Darby <sdarby@izeni.com>
2013-09-09 13:43:23 -0600

    Added aware date times to planet_update_all_feeds

--- planet/management/commands/planet_update_all_feeds.py
+++ planet/management/commands/planet_update_all_feeds.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 from django.core.management.base import NoArgsCommand
+import pytz
 
 from planet.management.commands import process_feed
 from planet.models import Feed
@@ -13,11 +14,11 @@ class Command(NoArgsCommand):
 
     def handle(self, *args, **options):
         new_posts_count = 0
-        start = datetime.now()
+        start = datetime.utcnow().replace(tzinfo=pytz.utc)
         for feed_url in Feed.site_objects.all().values_list("url", flat=True):
             # process feed in create-mode
             new_posts_count += process_feed(feed_url, create=False)
-        delta = datetime.now() - start
+        delta = datetime.now.utcnow().replace(tzinfo=pytz.utc) - start
         print "Added %s posts in %d seconds" % (new_posts_count, delta.seconds)
         feeds_updated.send(sender=self, instance=self)
 
