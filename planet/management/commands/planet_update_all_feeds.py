e37460db64bd4e5fa6aed0915c054401d27fbe8e
Stephen Darby <sdarby@izeni.com>
2013-09-09 13:56:18 -0600

    test

--- planet/management/commands/planet_update_all_feeds.py
+++ planet/management/commands/planet_update_all_feeds.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import datetime
 from django.core.management.base import NoArgsCommand
+import datetime
 from django.utils.timezone import utc
 
 from planet.management.commands import process_feed
@@ -18,7 +18,7 @@ class Command(NoArgsCommand):
         for feed_url in Feed.site_objects.all().values_list("url", flat=True):
             # process feed in create-mode
             new_posts_count += process_feed(feed_url, create=False)
-        delta = datetime.datetime.now.utcnow().replace(tzinfo=utc) - start
+        delta = datetime.datetime.utcnow().replace(tzinfo=utc) - start
         print "Added %s posts in %d seconds" % (new_posts_count, delta.seconds)
         feeds_updated.send(sender=self, instance=self)
 
